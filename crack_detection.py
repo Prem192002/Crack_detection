a55af6847199933ab189d4004730d3b59b683c79
Prem192002 <premsingh192002@gmail.com>
2023-05-08 11:12:08 +0530

    decreased the threshold, line gap increased

--- crack_detection.py
+++ crack_detection.py
@@ -1,19 +1,27 @@
 import cv2
 import numpy as np
 
-
+# Load the image
 img = cv2.imread(r"C:\Users\Prem\OneDrive\Pictures\crack_road2.jpeg")
 
+# Convert the image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+# Apply Gaussian blur to reduce noise
+blur = cv2.GaussianBlur(gray, (3, 3), 0)
 
-lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=100, minLineLength=100, maxLineGap=5)
+# Apply Canny edge detection to detect edges
+edges = cv2.Canny(blur, 50, 150, apertureSize=3)
 
+# Apply Hough transform to detect lines
+lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=50, minLineLength=50, maxLineGap=10)
+
+# Draw the detected lines on the original image
 for line in lines:
     x1, y1, x2, y2 = line[0]
-    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-cv2.imshow('Result', img)
+# Display the result
+cv2.imshow("Crack Detection", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
