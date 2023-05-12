5e23e25732cc0c4a5a7ed1be5bd00bb6ed12ac19
Prem192002 <premsingh192002@gmail.com>
2023-05-12 21:51:43 +0530

    first commit

--- LSD_algo2.py
+++ LSD_algo2.py
@@ -18,15 +18,18 @@ lsd = cv2.createLineSegmentDetector()
 lines, width, prec, nfa = lsd.detect(edges)
 
 # Draw the detected line segments on the original image
-for line in lines:
+for i, line in enumerate(lines):
     x1, y1, x2, y2 = map(int, line[0])
     thickness = 2
-    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), thickness if isinstance(thickness, float) else int(thickness))
+    if width[i] > 3:
+        color = (0, 255, 255) # red for large cracks
+    elif width[i] > 1:
+        color = (0, 0, 255) # yellow for medium cracks
+    else:
+        color = (0, 255, 0) # green for small cracks
+    cv2.line(img, (x1, y1), (x2, y2), color, thickness)
 
 # Display the result
 cv2.imshow("Crack Detection", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
