c8de32516222d9b900932d61b881c85d110df1c7
Prem192002 <premsingh192002@gmail.com>
2023-05-12 22:00:40 +0530

    decresd the line thichness

--- LSD_algo2.py
+++ LSD_algo2.py
@@ -20,14 +20,14 @@ lines, width, prec, nfa = lsd.detect(edges)
 # Draw the detected line segments on the original image
 for i, line in enumerate(lines):
     x1, y1, x2, y2 = map(int, line[0])
-    thickness = 2
-    if width[i] > 3:
-        color = (0, 255, 255) # red for large cracks
-    elif width[i] > 1:
-        color = (0, 0, 255) # yellow for medium cracks
+    thickness = width[i]
+    if thickness >= 3:
+        color = (0, 255, 255) # red for cracks with thickness between 5-3
+    elif thickness >= 1:
+        color = (0, 0, 255) # yellow for cracks with thickness between 3-1
     else:
-        color = (0, 255, 0) # green for small cracks
-    cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+        color = (0, 255, 0) # green for cracks with thickness less than 1
+    cv2.line(img, (x1, y1), (x2, y2), color, 2)
 
 # Display the result
 cv2.imshow("Crack Detection", img)
