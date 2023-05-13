1732ee1bd1f907b94beead65a507dcb88151ef52
Prem192002 <premsingh192002@gmail.com>
2023-05-13 22:46:47 +0530

    decresed the line thickness

--- probabilistic_Hough_transform .py
+++ probabilistic_Hough_transform .py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Load the image
-img = cv2.imread(r"C:\Users\Prem\OneDrive\Pictures\crack_road3.jpg")
+img = cv2.imread(r"C:\Users\Prem\OneDrive\Pictures\univ5.jpg")
 
 # Convert the image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -19,7 +19,7 @@ lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=50, minLineLe
 # Draw the detected lines on the original image
 for line in lines:
     x1, y1, x2, y2 = line[0]
-    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
 # Display the result
 cv2.imshow('Crack Detection', img)
