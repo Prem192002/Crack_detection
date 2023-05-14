2b20888c163cdabbb72fa47bf039dec4ee9f2c27
Prem192002 <premsingh192002@gmail.com>
2023-05-14 23:19:24 +0530

    canny value

--- LSD_algo2.py
+++ LSD_algo2.py
@@ -8,10 +8,10 @@ img = cv2.imread(r"C:\Users\Prem\OneDrive\Pictures\univ5.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Apply Gaussian blur to reduce noise
-blur = cv2.GaussianBlur(gray, (5,5), 0)
+blur = cv2.GaussianBlur(gray, (3, 3), 0)
 
 # Apply Canny edge detection to detect edges
-edges = cv2.Canny(blur, 0, 500, apertureSize=7)
+edges = cv2.Canny(blur, 10, 600, apertureSize=7)
 
 # Apply LSD algorithm to detect line segments
 lsd = cv2.createLineSegmentDetector()
