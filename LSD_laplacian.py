419fe58246969fe14cd6d3ef8f18c9f24501ebbd
Prem192002 <premsingh192002@gmail.com>
2023-05-15 22:30:36 +0530

    noice reduced

--- LSD_laplacian.py
+++ LSD_laplacian.py
@@ -8,7 +8,7 @@ img = cv2.imread(r"C:\Users\Prem\OneDrive\Pictures\sample4.jpeg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Apply Gaussian blur to reduce noise
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
+blur = cv2.GaussianBlur(gray, (7, 5), 0)
 
 # Apply Laplacian edge detection
 laplacian = cv2.Laplacian(blur, cv2.CV_64F)
