3952b91190663b562296a145d5dd1d5cfd811f49
Prem192002 <premsingh192002@gmail.com>
2023-05-19 22:20:33 +0530

    applied top-hat transform

--- probabilistic_Hough_transform .py
+++ probabilistic_Hough_transform .py
@@ -8,27 +8,34 @@ img = cv2.imread(r"C:\Users\Prem\OneDrive\Pictures\sample4.jpeg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Apply Gaussian blur to reduce noise
-blur = cv2.GaussianBlur(gray, (5,5), 0)
+blur = cv2.GaussianBlur(gray, (5, 5), 2)
 
-# Apply Laplacian edge detection
-laplacian = cv2.Laplacian(blur, cv2.CV_64F)
+# Apply top-hat transform to enhance cracks
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
+tophat = cv2.morphologyEx(blur, cv2.MORPH_TOPHAT, kernel)
 
-# Convert the Laplacian result to uint8
-laplacian = np.uint8(np.absolute(laplacian))
+# Apply Scharr edge detection to the top-hat image
+gradient_x = cv2.Scharr(tophat, cv2.CV_64F, 1, 0)
+gradient_y = cv2.Scharr(tophat, cv2.CV_64F, 0, 1)
+gradient_abs = cv2.addWeighted(cv2.convertScaleAbs(gradient_x), 0.5,
+                               cv2.convertScaleAbs(gradient_y), 0.5, 0)
 
 # Apply thresholding to detect edges
-edges = cv2.threshold(laplacian, 0, 250, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+edges = cv2.threshold(gradient_abs, 50, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-# Apply LSD algorithm to detect line segments
-lsd = cv2.createLineSegmentDetector()
-lines, width, prec, nfa = lsd.detect(edges)
+# Perform morphological closing
+kernel = np.ones((5, 5), np.uint8)
+edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+
+# Apply Hough Line Transform for line segment detection
+lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=50, minLineLength=50, maxLineGap=10)
 
 # Draw the detected line segments on the original image
-for i, line in enumerate(lines):
-    x1, y1, x2, y2 = map(int, line[0])
-    thickness = width[i]
-    color = (0, 0, 255)  # Red color for all cracks
-    cv2.line(img, (x1, y1), (x2, y2), color, 2)
+if lines is not None:
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        color = (0, 0, 255)  # Red color for all cracks
+        cv2.line(img, (x1, y1), (x2, y2), color, 2)
 
 # Display the result
 cv2.imshow("Crack Detection", img)
