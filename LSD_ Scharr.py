dd756e54874fc6ef6be75da4709e2ab23879e5e1
Prem192002 <premsingh192002@gmail.com>
2023-05-28 23:24:16 +0530

    set the blur value to 2

--- LSD_ Scharr.py
+++ LSD_ Scharr.py
@@ -11,7 +11,7 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (3,3), 1)
 
 # Apply Scharr edge detection
-gradient_x = cv2.Scharr(blur, cv2.CV_64F, 1, 0)
+gradient_x = cv2.Scharr(blur, cv2.CV_64F, 1, 0) 
 gradient_y = cv2.Scharr(blur, cv2.CV_64F, 0, 1)
 gradient_abs = cv2.addWeighted(cv2.convertScaleAbs(gradient_x), 0.5,
                                cv2.convertScaleAbs(gradient_y), 0.5, 1)
@@ -29,14 +29,14 @@ for i, line in enumerate(lines):
     thickness = width[i]
     
     # Set the color based on crack size
-    if thickness > 5:
-        color = (0, 255, 255)  # Red for larger cracks
-    elif thickness > 2:
-        color = (0, 0, 255)  # Yellow for medium cracks
+    if thickness > 3:
+        color = (0, 0, 255)  # Red for larger cracks
+    elif thickness > 1:
+        color = (0, 255, 255)  # Yellow for medium cracks
     else:
         color = (0, 255, 0)  # Green for small cracks
     
-    cv2.line(img, (x1, y1), (x2, y2), color, 1)
+    cv2.line(img, (x1, y1), (x2, y2), color, 2)
 
 # Display the result
 cv2.imshow("Crack Detection", img)
