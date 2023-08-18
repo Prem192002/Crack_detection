b5236d46ac825810464a6173b1adf39d88adf666
Prem192002 <premsingh192002@gmail.com>
2023-08-18 23:49:32 +0530

    code set changed

--- crack_detection.py
+++ crack_detection.py
@@ -8,20 +8,17 @@ img = cv2.imread(r"C:\Users\Prem\OneDrive\Pictures\sample4.jpeg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Apply Gaussian blur to reduce noise
-blur = cv2.GaussianBlur(gray, (3,3), 2)
+blur = cv2.GaussianBlur(gray, (3, 3), 2)
 
 # Apply Canny edge detection to detect edges
-edges = cv2.Canny(blur, 50,500, apertureSize=3)
+edges = cv2.Canny(blur, 50, 150, apertureSize=3)  # Adjusted upper threshold
 
 # Apply Hough transform to detect lines
-lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=50, minLineLength=50, maxLineGap=10)
+lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=50, minLineLength=50, maxLineGap=5)  # Adjusted parameters
 
-# Draw the detected lines on the original image
-for line in lines:
-    x1, y1, x2, y2 = line[0]
-    cv2.line(img, (x1, y1), (x2, y2), (0, 250, 255) ,1)
-
-# Display the result
-cv2.imshow("Crack Detection", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Check if lines were detected
+if lines is not None:
+    # Draw the detected lines on the original image
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 1) 
